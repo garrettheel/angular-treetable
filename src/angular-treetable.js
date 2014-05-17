bb69939e5d11e7a097891366c2029fae6a6a1b00
Garrett Heel <garrettheel@gmail.com>
2014-05-17 18:15:27 +1000

    Move logic into a controller

--- src/angular-treetable.js
+++ src/angular-treetable.js
@@ -2,7 +2,66 @@
 
 angular.module('ngTreetable', [])
 
-    .directive('ttTable', ['$compile', '$templateCache', function($compile, $templateCache) {
+    .controller('TreetableController', ['$scope', '$element', '$compile', '$templateCache', function($scope, $element, $compile, $templateCache) {
+
+        $scope.compileElement = function(node, parentId) {
+            var tpl = angular.isFunction($scope.template) ? $scope.template(node) : $scope.template;
+            var template = $templateCache.get(tpl);
+            var template_scope = $scope.$parent.$new();
+            template_scope.node = node;
+            template_scope._ttParent = parentId;
+            return $compile(template)(template_scope);
+        }
+
+        $scope.addChildren = function(parentElement) {
+            var parentNode = parentElement ? parentElement.scope().node : null;
+            var parentId = parentElement ? parentElement.data('ttId') : null;
+
+            if (parentElement) {
+                parentElement.scope().loading = true;
+            }
+
+            $scope.nodes(parentNode).then(function(data) {
+                var newElements = [];
+                angular.forEach(data, function(node) {
+                    var row = $scope.compileElement(node, parentId);
+                    newElements.push(row.get(0));
+                });
+
+                var parentTtNode = parentId != null ? $element.treetable("node", parentId) : null;
+                $element.treetable('loadBranch', parentTtNode, newElements);
+
+                if (parentElement) parentElement.scope().loading = false;
+
+                if (parentElement == null && angular.isFunction($scope.afterInit)) {
+                    $scope.afterInit();
+                }
+            });
+        }
+
+        $scope.onNodeExpand = function() {
+            if (this.row.scope().loading) return; // make sure we're not already loading
+            $element.treetable('unloadBranch', this); // make sure we don't double-load
+            $scope.addChildren(this.row);
+        }
+
+        $scope.onNodeCollapse = function() {
+            if (this.row.scope().loading) return; // make sure we're not already loading
+            $element.treetable('unloadBranch', this);
+        }
+
+        $element.treetable({
+            expandable: true,
+            onNodeExpand: $scope.onNodeExpand,
+            onNodeCollapse: $scope.onNodeCollapse
+        });
+
+        $scope.addChildren(null);
+
+
+    }])
+
+    .directive('ttTable', [function() {
         return {
             restrict: 'EAC',
             scope: {
@@ -10,63 +69,7 @@ angular.module('ngTreetable', [])
                 nodes: '=',
                 afterInit: '='
             },
-            link: function(scope, element) {
-
-                function compileElement(node, parentId) {
-                    var tpl = angular.isFunction(scope.template) ? scope.template(node) : scope.template;
-                    var template = $templateCache.get(tpl);
-                    var template_scope = scope.$parent.$new();
-                    template_scope.node = node;
-                    template_scope._ttParent = parentId;
-                    return $compile(template)(template_scope);
-                }
-
-                function addChildren(parentElement) {
-                    var parentNode = parentElement ? parentElement.scope().node : null;
-                    var parentId = parentElement ? parentElement.data('ttId') : null;
-
-                    if (parentElement) {
-                        parentElement.scope().loading = true;
-                    }
-
-                    scope.nodes(parentNode).then(function(data) {
-                        var newElements = [];
-                        angular.forEach(data, function(node) {
-                            var row = compileElement(node, parentId);
-                            newElements.push(row.get(0));
-                        });
-
-                        var parentTtNode = parentId != null ? element.treetable("node", parentId) : null;
-                        element.treetable('loadBranch', parentTtNode, newElements);
-
-                        if (parentElement) parentElement.scope().loading = false;
-
-                        if (parentElement == null && angular.isFunction(scope.afterInit)) {
-                            scope.afterInit();
-                        }
-                    });
-                }
-
-                function onNodeExpand() {
-                    if (this.row.scope().loading) return; // make sure we're not already loading
-                    element.treetable('unloadBranch', this); // make sure we don't double-load
-                    addChildren(this.row);
-                }
-
-                function onNodeCollapse() {
-                    if (this.row.scope().loading) return; // make sure we're not already loading
-                    element.treetable('unloadBranch', this);
-                }
-
-                element.treetable({
-                    expandable: true,
-                    onNodeExpand: onNodeExpand,
-                    onNodeCollapse: onNodeCollapse
-                });
-
-                addChildren(null);
-
-            }
+            controller: 'TreetableController'
         }
     }])
 
